231231da9905becca06384562e324e5e2fdfa091
khun84 <khun84@gmail.com>
2020-07-30 14:23:26 +0800

    exclude admin log entry import by controller actions

--- lib/admin_log_importer.rb
+++ lib/admin_log_importer.rb
@@ -1,11 +1,15 @@
 class AdminLogImporter
-  attr_reader :es_client, :filepath
-  def self.run(args= {})
+  attr_reader :es_client, :filepath, :exclude_actions
+  def self.run(args = {})
     new(**args).run
   end
 
-  def initialize(filepath:)
-    @filepath = filepath
+  # @param [Hash] args
+  #   @option [String] filepath
+  #   @option [Array<String>] exclude_actions
+  def initialize(args = {})
+    @filepath = args[:filepath]
+    @exclude_actions = Array(args[:exclude_actions])
     @es_client = EsClient.new(index_name: :admin_log)
   end
 
@@ -21,10 +25,12 @@ class AdminLogImporter
 
     File.open(filepath, mode: 'r').each_with_index do |row, idx|
       next if idx == 0
-      total_count += 1
       row = JSON.parse(row)
+      next if row['action'].in?(exclude_actions)
       row['timestamp'] = row['@timestamp']
       row['params'] = row['params'].to_json
+
+      total_count += 1
       raw_payload.push(row)
       if (idx % 50) == 0
         puts "----------importing #{raw_payload.size} documents-------------"
